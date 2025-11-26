b0a33c14d337ee29b76ba0be224dc9d0ee02efc6
scottwestover <scottwestover2006@gmail.com>
2025-11-26 16:12:07 -0500

    added core sokoban logic
    Signed-off-by: Scott Westover <swestover@manh.com>

--- src/lib/SokobanGame.js
+++ src/lib/SokobanGame.js
@@ -5,6 +5,25 @@
  * @property {number} y
  */
 
+/**
+ * @typedef SokobanGameState
+ * @type {object}
+ * @property {Coordinate} playerPosition
+ * @property {number[][]} map
+ * @property {Coordinate[]} boxPositions
+ * @property {Coordinate[]} goalPositions
+ * @property {boolean} isWon
+ * @property {number} width
+ * @property {number} height
+ */
+
+const DIRECTIONS = Object.freeze({
+  up: { x: 0, y: -1 },
+  down: { x: 0, y: 1 },
+  left: { x: -1, y: 0 },
+  right: { x: 1, y: 0 },
+});
+
 export const TILE_TYPES = Object.freeze({
   EMPTY: 0,
   FLOOR: 1,
@@ -83,31 +102,113 @@ export class SokobanGame {
   }
 
   /**
+   *
    * @param {string} direction - up, down, left, right
    * @returns {boolean} True if move was successful, false if blocked
    */
   movePlayer(direction) {
-    // TODO
+    const dir = DIRECTIONS[direction];
+    if (dir === undefined) {
+      return false;
+    }
+
+    const newX = this.#playerPosition.x + dir.x;
+    const newY = this.#playerPosition.y + dir.y;
+
+    if (!this.#canMoveToPosition(newX, newY)) {
+      const targetTile = this.#currentMap[newY] && this.#currentMap[newY][newX];
+      if (targetTile === TILE_TYPES.BOX || targetTile === TILE_TYPES.BOX_ON_GOAL) {
+        return this.#pushBox(newX, newY, newX + dir.x, newY + dir.y);
+      }
+      return false;
+    }
+
+    this.#playerPosition.x = newX;
+    this.#playerPosition.y = newY;
+    return true;
+  }
+
+  #isValidPosition(x, y) {
+    return x >= 0 && x < this.#width && y >= 0 && y < this.#height;
+  }
+
+  #canMoveToPosition(x, y) {
+    if (!this.#isValidPosition(x, y)) {
+      return false;
+    }
+
+    const tile = this.#currentMap[y][x];
+    return tile === TILE_TYPES.FLOOR || tile === TILE_TYPES.GOAL;
+  }
+
+  #pushBox(boxX, boxY, pushToX, pushToY) {
+    if (!this.#canMoveToPosition(pushToX, pushToY)) {
+      return false;
+    }
+
+    const currentBoxTile = this.#currentMap[boxY][boxX];
+    if (currentBoxTile === TILE_TYPES.BOX_ON_GOAL) {
+      this.#currentMap[boxY][boxX] = TILE_TYPES.GOAL;
+    } else {
+      this.#currentMap[boxY][boxX] = TILE_TYPES.FLOOR;
+    }
+
+    const targetTile = this.#currentMap[pushToY][pushToX];
+    if (targetTile === TILE_TYPES.GOAL) {
+      this.#currentMap[pushToY][pushToX] = TILE_TYPES.BOX_ON_GOAL;
+    } else {
+      this.#currentMap[pushToY][pushToX] = TILE_TYPES.BOX;
+    }
+
+    this.#playerPosition.x = boxX;
+    this.#playerPosition.y = boxY;
+
+    const boxIndex = this.#boxPositions.findIndex(box => box.x === boxX && box.y === boxY);
+    if (boxIndex !== -1) {
+      this.#boxPositions[boxIndex] = { x: pushToX, y: pushToY };
+    }
+
+    return true;
   }
 
   /**
    * @returns {void}
    */
   reset() {
-    // TODO
+    this.#currentMap = this.#originalMap.map(row => [...row]);
+
+    this.#playerPosition = this.#findPlayerStartPosition();
+    this.#currentMap[this.#playerPosition.y][this.#playerPosition.x] = TILE_TYPES.FLOOR;
+
+    this.#boxPositions = this.#findBoxPositions();
   }
 
   /**
    * @returns {boolean}
    */
   isWinCondition() {
-    // TODO
+    for (const box of this.#boxPositions) {
+      const tile = this.#currentMap[box.y][box.x];
+      if (tile !== TILE_TYPES.BOX_ON_GOAL) {
+        return false;
+      }
+    }
+    return true;
   }
 
   /**
 	 * Returns a complete snapshot of the current game state
+	 * @returns {SokobanGameState} Game state object with all current information
 	 */
   getGameState() {
-    return {};
+    return {
+      playerPosition: {...this.#playerPosition},
+      map: this.#currentMap.map(row => [...row]),
+      boxPositions: this.#boxPositions.map(box => ({...box})),
+      goalPositions: this.#goalPositions.map(goal => ({...goal})),
+      isWon: this.isWinCondition(),
+      width: this.#width,
+      height: this.#height,
+    };
   }
 }
