67bd7cdcec6eb231f92290d58111a27dca703909
scottwestover <scottwestover2006@gmail.com>
2025-11-26 12:06:00 -0500

    added assets to project
    Signed-off-by: Scott Westover <swestover@manh.com>

--- src/main.js
+++ src/main.js
@@ -1,33 +1,21 @@
-import Level from "./scenes/Level.js";
 import Preload from "./scenes/Preload.js";
+import Level from "./scenes/Level.js";
 
 window.addEventListener('load', function () {
 
 	var game = new Phaser.Game({
-		width: 1280,
-		height: 720,
+		width: 640,
+		height: 480,
 		type: Phaser.AUTO,
-        backgroundColor: "#242424",
+		backgroundColor: "#584422",
+		pixelArt: true,
 		scale: {
 			mode: Phaser.Scale.FIT,
 			autoCenter: Phaser.Scale.CENTER_BOTH
-		}
+		},
+		banner: false
 	});
 
-	game.scene.add("Preload", Preload);
 	game.scene.add("Level", Level);
-	game.scene.add("Boot", Boot, true);
+	game.scene.add("Preload", Preload, true);
 });
-
-class Boot extends Phaser.Scene {
-
-	preload() {
-		
-		this.load.pack("pack", "assets/preload-asset-pack.json");
-	}
-
-	create() {
-
-		this.scene.start("Preload");
-	}
-}
--- src/scenes/Level.js
+++ src/scenes/Level.js
@@ -19,26 +19,8 @@ export default class Level extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 
-		// dino
-		const dino = this.add.image(640, 288, "dino");
-		dino.setInteractive(new Phaser.Geom.Rectangle(0, 0, 250, 250), Phaser.Geom.Rectangle.Contains);
-
-		// welcome
-		const welcome = this.add.text(640, 478, "", {});
-		welcome.setOrigin(0.5, 0.5);
-		welcome.text = "Phaser 3 + Phaser Editor v4";
-		welcome.setStyle({ "fontFamily": "Arial", "fontSize": "30px" });
-
-		this.dino = dino;
-		this.welcome = welcome;
-
 		this.events.emit("scene-awake");
 	}
-
-	/** @type {Phaser.GameObjects.Image} */
-	dino;
-	/** @type {Phaser.GameObjects.Text} */
-	welcome;
 
 	/* START-USER-CODE */
 
@@ -48,10 +30,6 @@ export default class Level extends Phaser.Scene {
 
 		this.editorCreate();
 
-		this.dino.on("pointerdown", () => {
-
-			this.welcome.text = "Hello, World!";
-		});
 	}
 
 	/* END-USER-CODE */
