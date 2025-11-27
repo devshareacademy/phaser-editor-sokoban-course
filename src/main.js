887585f457a88ff45665b24ffad67338a896da10
scottwestover <scottwestover2006@gmail.com>
2025-11-26 19:58:18 -0500

    added 2nd and 3rd levels
    Signed-off-by: Scott Westover <swestover@manh.com>

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Boot from "./scenes/Boot.js";
 import Preload from "./scenes/Preload.js";
 import Title from "./scenes/Title.js";
 import Level1 from "./scenes/Level1.js";
+import Level2 from "./scenes/Level2.js";
+import Level3 from "./scenes/Level3.js";
 
 window.addEventListener('load', function () {
 
@@ -21,5 +23,7 @@ window.addEventListener('load', function () {
 	game.scene.add("Preload", Preload);
 	game.scene.add("Title", Title);
 	game.scene.add("Level1", Level1);
+	game.scene.add("Level2", Level2);
+	game.scene.add("Level3", Level3);
 	game.scene.add("Boot", Boot, true);
 });
---
+++ src/scenes/Level3.js
@@ -0,0 +1,221 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import GameManagerScript from "../scripts/GameManagerScript.js";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class Level3 extends Phaser.Scene {
+
+	constructor() {
+		super("Level3");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/** @returns {void} */
+	editorCreate() {
+
+		// editabletilemap
+		this.cache.tilemap.add("editabletilemap_a6154d9b-7c6a-4846-9869-084620d2c56a", {
+			format: 1,
+			data: {
+				width: 12,
+				height: 12,
+				orientation: "orthogonal",
+				tilewidth: 16,
+				tileheight: 16,
+				tilesets: [
+					{
+						columns: 14,
+						margin: 0,
+						spacing: 0,
+						tilewidth: 16,
+						tileheight: 16,
+						tilecount: 224,
+						firstgid: 1,
+						image: "walls",
+						name: "walls",
+						imagewidth: 224,
+						imageheight: 256,
+					},
+					{
+						columns: 11,
+						margin: 0,
+						spacing: 0,
+						tilewidth: 16,
+						tileheight: 16,
+						tilecount: 88,
+						firstgid: 225,
+						image: "animated_props",
+						name: "animated_props",
+						imagewidth: 176,
+						imageheight: 128,
+					},
+					{
+						columns: 7,
+						margin: 0,
+						spacing: 0,
+						tilewidth: 16,
+						tileheight: 16,
+						tilecount: 63,
+						firstgid: 313,
+						image: "elf",
+						name: "elf",
+						imagewidth: 112,
+						imageheight: 144,
+					},
+					{
+						columns: 13,
+						margin: 0,
+						spacing: 0,
+						tilewidth: 16,
+						tileheight: 16,
+						tilecount: 208,
+						firstgid: 376,
+						image: "grounds",
+						name: "grounds",
+						imagewidth: 208,
+						imageheight: 256,
+					},
+					{
+						columns: 19,
+						margin: 0,
+						spacing: 0,
+						tilewidth: 16,
+						tileheight: 16,
+						tilecount: 152,
+						firstgid: 584,
+						image: "props",
+						name: "props",
+						imagewidth: 304,
+						imageheight: 128,
+					},
+				],
+				layers: [
+					{
+						type: "tilelayer",
+						name: "floorTileLayer",
+						width: 12,
+						height: 12,
+						opacity: 1,
+						data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 436, 438, 438, 438, 438, 439, 0, 0, 0, 0, 0, 0, 449, 463, 463, 463, 463, 465, 0, 0, 0, 0, 0, 0, 449, 463, 463, 463, 463, 465, 0, 0, 0, 0, 0, 0, 449, 463, 463, 463, 463, 465, 0, 0, 0, 0, 0, 0, 475, 476, 476, 476, 476, 478, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					},
+					{
+						type: "tilelayer",
+						name: "wallTileLayer",
+						width: 12,
+						height: 12,
+						opacity: 1,
+						data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 44, 4, 4, 4, 4, 4, 4, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 32, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 32, 0, 0, 0, 0, 29, 30, 30, 30, 30, 30, 30, 46, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					},
+					{
+						type: "tilelayer",
+						name: "playerTileLayer",
+						width: 12,
+						height: 12,
+						opacity: 1,
+						data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 313, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					},
+					{
+						type: "tilelayer",
+						name: "blockTileLayer",
+						width: 12,
+						height: 12,
+						opacity: 1,
+						data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 605, 0, 0, 605, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 605, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					},
+					{
+						type: "tilelayer",
+						name: "goalTileLayer",
+						width: 12,
+						height: 12,
+						opacity: 1,
+						data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 251, 0, 0, 0, 251, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 251, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					},
+					{
+						type: "tilelayer",
+						name: "blockingTileLayer",
+						width: 12,
+						height: 12,
+						opacity: 1,
+						data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 606, 0, 0, 606, 0, 0, 0, 0, 0, 0, 0, 0, 0, 606, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+					},
+				],
+			},
+		});
+		const editabletilemap = this.add.tilemap("editabletilemap_a6154d9b-7c6a-4846-9869-084620d2c56a");
+		editabletilemap.addTilesetImage("walls");
+		editabletilemap.addTilesetImage("animated_props");
+		editabletilemap.addTilesetImage("elf");
+		editabletilemap.addTilesetImage("grounds");
+		editabletilemap.addTilesetImage("props");
+
+		// wallTileLayer
+		const wallTileLayer = editabletilemap.createLayer("wallTileLayer", ["walls"], 128, 48);
+		wallTileLayer.scaleX = 2;
+		wallTileLayer.scaleY = 2;
+
+		// floorTileLayer
+		const floorTileLayer = editabletilemap.createLayer("floorTileLayer", ["grounds"], 128, 48);
+		floorTileLayer.scaleX = 2;
+		floorTileLayer.scaleY = 2;
+
+		// playerTileLayer
+		const playerTileLayer = editabletilemap.createLayer("playerTileLayer", ["elf"], 128, 48);
+		playerTileLayer.scaleX = 2;
+		playerTileLayer.scaleY = 2;
+
+		// blockTileLayer
+		const blockTileLayer = editabletilemap.createLayer("blockTileLayer", ["props"], 128, 48);
+		blockTileLayer.scaleX = 2;
+		blockTileLayer.scaleY = 2;
+
+		// goalTileLayer
+		const goalTileLayer = editabletilemap.createLayer("goalTileLayer", ["animated_props"], 128, 48);
+		goalTileLayer.scaleX = 2;
+		goalTileLayer.scaleY = 2;
+
+		// blockingTileLayer
+		const blockingTileLayer = editabletilemap.createLayer("blockingTileLayer", ["props"], 128, 48);
+		blockingTileLayer.scaleX = 2;
+		blockingTileLayer.scaleY = 2;
+
+		// gameManagerScript
+		const gameManagerScript = new GameManagerScript(this);
+
+		// gameManagerScript (prefab fields)
+		gameManagerScript.wallTileLayer = wallTileLayer;
+		gameManagerScript.blockTileLayer = blockTileLayer;
+		gameManagerScript.floorTileLayer = floorTileLayer;
+		gameManagerScript.playerTileLayer = playerTileLayer;
+		gameManagerScript.goalTileLayer = goalTileLayer;
+		gameManagerScript.blockingTileLayer = blockingTileLayer;
+
+		this.editabletilemap = editabletilemap;
+
+		this.events.emit("scene-awake");
+	}
+
+	/** @type {Phaser.Tilemaps.Tilemap} */
+	editabletilemap;
+
+	/* START-USER-CODE */
+
+	// Write your code here
+
+	create() {
+
+		this.editorCreate();
+	}
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
